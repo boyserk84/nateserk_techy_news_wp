4a9ca9a50cc0db8112394c09b64cba9df302f104
Nate Kem <natek@Nates-MacBook>
2017-08-31 23:33:32 -0700

    Fix centering elements.

--- single.php
+++ single.php
@@ -17,7 +17,7 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			get_template_part( 'template-parts/content', 'navigation' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
