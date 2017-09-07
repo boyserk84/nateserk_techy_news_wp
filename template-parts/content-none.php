78b0ba4b085d567eb17e1f6a2c1b8ac42a7e6c47
Nate Kem <natek@Nates-MacBook>
2017-09-06 22:18:50 -0700

    [ Misc Template #13 ] Fix the issue with template-none not centering.

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -9,12 +9,12 @@
 
 ?>
 
-<section class="no-results not-found">
-	<header class="page-header">
+<section class="no-results not-found pure-g pure-u-1">
+	<header class="page-header pure-u-1">
 		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'nateserk_techy_news' ); ?></h1>
 	</header><!-- .page-header -->
 
-	<div class="page-content">
+	<div class="page-content pure-u-1 pure-md-1 pure-sm-1">
 		<?php
 		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
@@ -34,7 +34,6 @@
 			?></p>
 
 		<?php elseif ( is_search() ) : ?>
-
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'nateserk_techy_news' ); ?></p>
 			<?php
 				get_search_form();
