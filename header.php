76c1caea90abb44a8a35397683558b339cc5d39c
Nate Kem <natek@Nates-MacBook>
2017-09-11 22:44:37 -0700

    [ Adhoc issue #8 ] Fix the issue with header and menu search form space.

--- header.php
+++ header.php
@@ -37,13 +37,17 @@ do_action( 'nateserk_techy_news_action_before_head' );
 	<header id="masthead" class="site-header">
 		<!--header-->
 				<div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed site-primary">
-						<a class="site-title pure-menu-heading pure-u-md-1-5 site-primary" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php the_custom_logo(); bloginfo( 'name' );?></a>
-						<ul class="pure-menu-list pure-u-md-3-5"><?php do_action('nateserk_techy_news_action_setup_menu', 'menu-header'); ?>
-						</ul>
-						<div class="content-center pure-u-sm-1 pure-u-md-1-5 pure-u-1-5">
-							<?php do_action( 'nateserk_technynews_action_show_social_links' ); ?>
-							<?php get_search_form(); ?>
-						</div>
+						<a class="site-title pure-menu-heading pure-u-md-5-24 pure-u-5-24 site-primary" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php the_custom_logo(); bloginfo( 'name' );?></a>
+						<ul class="pure-menu-list pure-u-12-24 pure-u-md-12-24"><?php do_action('nateserk_techy_news_action_setup_menu', 'menu-header'); ?>
+						</ul><!-- menu -->
+						<div class="pure-u-sm-1 pure-u-md-8-24 pure-u-8-24">
+							<div class="pure-u-sm-1 pure-u-md-1-5 pure-u-1-5" style="margin-right:1.5em;">
+								<?php do_action( 'nateserk_technynews_action_show_social_links' ); ?>
+							</div>
+							<div class="pure-u-sm-1 pure-u-md-1-5 pure-u-1-5">
+								<?php get_search_form(); ?>
+							</div>
+						</div><!-- Social links & search -->
 				</div>
 		<!--header-->
 	</header><!-- #masthead -->
--- searchform.php
+++ searchform.php
@@ -5,10 +5,10 @@
 * Customize search form according to https://developer.wordpress.org/reference/functions/get_search_form/.
 */
 ?>
-<form role="search" method="get" class="search-form pure-u-1 pure-sm-1" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 				<label>
 					<span class="screen-reader-text">Search for:</span>
-					<input type="search" class="search-field" placeholder="Search &hellip;" value="" name="s" required="required"/>
+					<input type="search" class="search-field" placeholder="Search &hellip;" size=15 value="" name="s" required="required"/>
 				</label>
 				<button type="submit" class="pure-button site-primary" value="Search"><i class="fa fa-search" aria-hidden="true"></i></button>
 </form>
