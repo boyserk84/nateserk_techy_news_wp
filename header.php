7ef90223c997e47629501a0c58a4418ab5345472
Nate Kem <natek@Nates-MacBook>
2017-09-13 23:07:22 -0700

    [ Adhoc Issue #8 ] Fix the issue with the header menu not vertically aligned in the middle with the custom logo image.

--- header.php
+++ header.php
@@ -37,14 +37,25 @@ do_action( 'nateserk_techy_news_action_before_head' );
 	<header id="masthead" class="site-header">
 		<!--header-->
 				<div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed site-primary">
-						<a class="site-title pure-menu-heading pure-u-md-5-24 pure-u-5-24 site-primary" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php the_custom_logo(); bloginfo( 'name' );?></a>
-						<ul class="pure-menu-list pure-u-12-24 pure-u-md-12-24"><?php do_action('nateserk_techy_news_action_setup_menu', 'menu-header'); ?>
+						<!-- TODO: P2 Put this header into its own template or customizable template-->
+						<div class="site-title pure-menu-heading pure-u-sm-5-24 pure-u-md-5-24 pure-u-5-24 site-primary">
+							<?php
+							 	if ( has_custom_logo() ) :
+									the_custom_logo();
+								else:?>
+									<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' );?></a>
+								<?php
+								endif;
+							?>
+						</div>
+
+						<ul class="pure-menu-list pure-u-sm-1 pure-u-12-24 pure-u-md-12-24" style="vertical-align:middle"><?php do_action('nateserk_techy_news_action_setup_menu', 'menu-header'); ?>
 						</ul><!-- menu -->
-						<div class="pure-u-sm-1 pure-u-md-8-24 pure-u-8-24">
+						<div class="pure-u-sm-1 pure-u-md-8-24 pure-u-8-24" style="vertical-align:middle">
 							<div class="pure-u-sm-1 pure-u-md-1-5 pure-u-1-5" style="margin-right:1.5em;">
 								<?php do_action( 'nateserk_technynews_action_show_social_links' ); ?>
 							</div>
-							<div class="pure-u-sm-1 pure-u-md-1-5 pure-u-1-5">
+							<div class="pure-u-sm-1 pure-u-md-1-5 pure-u-1-5" style="vertical-align:middle">
 								<?php get_search_form(); ?>
 							</div>
 						</div><!-- Social links & search -->
